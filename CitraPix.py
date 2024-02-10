acaf5262af9bcc42d4b81ff5be205cc583db2cd6
_userNull <ramzprivatexd@gmail.com>
2024-02-10 09:56:18 +0700

    add: load animation

--- CitraPix.py
+++ CitraPix.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder="./static", template_folder="./templates")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", testing_content="Anjay ini sudah di anu")
 
+@app.route("/deblurring")
+def deblurring():
+    return render_template("deblurring.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
 
